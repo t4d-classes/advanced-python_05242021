609a498d089bb21c60f18e9771f177aad4c99e4d
Eric Greene <eric@t4d.io>
2021-05-25 11:37:36 -0400

    add solution to exercise 2

--- python_demos/rates_demo/__main__.py
+++ python_demos/rates_demo/__main__.py
@@ -1,8 +1,31 @@
 """ main module """
+from datetime import date
+from requests import request
+
+from rates_demo.business_days import business_days
 
 
 def get_rates() -> None:
     """ get the rates """
+
+    start_date = date(2021, 1, 1)
+    end_date = date(2021, 3, 31)
+
+    rate_responses: list[str] = []
+
+    for business_day in business_days(start_date, end_date):
+
+        rate_url = "".join([
+            "http://127.0.0.1:5000/api/",
+            str(business_day),
+            "?base=USD&symbols=EUR"])
+
+        response = request("GET", rate_url)
+        rate_responses.append(response.text)
+
+    for rate_response in rate_responses:
+        print(rate_response)
+
 
 if __name__ == "__main__":
     get_rates()
