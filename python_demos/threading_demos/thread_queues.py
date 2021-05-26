d93ee2ad8b95237e7ec36c1f579f55acc46f5c1b
Eric Greene <eric@t4d.io>
2021-05-26 10:24:21 -0400

    add first rate server exercise

--- python_demos/threading_demos/thread_queues.py
+++ python_demos/threading_demos/thread_queues.py
@@ -15,9 +15,11 @@ double_nums_done = threading.Event()
 def generate_nums(number_of_nums: int, queue_nums: queue.Queue[int]) -> None:
     """ generate numbers """
 
+    generate_nums_done.clear()
+
     for _ in range(number_of_nums):
-        print("generate number")
         num = randint(1,11)
+        print(f"generate number: {num}")
         queue_nums.put(num)
         time.sleep(1)
 
@@ -27,6 +29,8 @@ def double_the_nums(
     queue_nums: queue.Queue[int],
     queue_double_nums: queue.Queue[int]) -> None:
     """ double numbers """
+
+    double_nums_done.clear()
 
     while True:
         try:
@@ -45,13 +49,14 @@ def output_nums(queue_double_nums: queue.Queue[int]) -> None:
     while True:
         try:
             num = queue_double_nums.get(timeout=0.1)
-            print(num)
+            print(f"output double: {num}")
         except queue.Empty:
             if double_nums_done.is_set():
                 break
             else:
                 continue
 
+print("running the first pipeline")
 
 generate_nums_thread = threading.Thread(target=generate_nums, args=(10, nums))
 double_the_nums_thread = threading.Thread(
@@ -61,3 +66,24 @@ output_nums_thread = threading.Thread(target=output_nums, args=(double_nums,))
 generate_nums_thread.start()
 double_the_nums_thread.start()
 output_nums_thread.start()
+
+generate_nums_thread.join()
+double_the_nums_thread.join()
+output_nums_thread.join()
+
+print("running the second pipeline")
+
+generate_nums_thread = threading.Thread(target=generate_nums, args=(10, nums))
+double_the_nums_thread = threading.Thread(
+    target=double_the_nums, args=(nums,double_nums))
+output_nums_thread = threading.Thread(target=output_nums, args=(double_nums,))
+
+generate_nums_thread.start()
+double_the_nums_thread.start()
+output_nums_thread.start()
+
+generate_nums_thread.join()
+double_the_nums_thread.join()
+output_nums_thread.join()
+
+print("all done")
